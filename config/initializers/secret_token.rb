63ed96045294a7eb8b054e5e7b37d75df17fe5b4
VitaleksPC <vitalekspc@ubuntu.(none)>
2013-02-27 19:38:31 +0400

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DayBook::Application.config.secret_token = 'ce5b22dc96262c613c53f1ffb1c3665c291044c2d71a675708f005b08dac0e2c5469e8f8862ccdd474d21e000a64916ffe54d709d5ecd2764fe82f19ebea6aee'
